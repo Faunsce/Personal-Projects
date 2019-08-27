bae800fa08123b8f43969577db81b817b36c3e40
Dominik Rutter <dominik.rutter@gmail.com>
2019-08-27 01:40:02 -0400

    CMake(over)

---
+++ Infinitefile/main.cpp
@@ -0,0 +1,10 @@
+// Includes-Usings
+#include <iostream>
+#include <fstream>
+// Variables-Data
+std::fstream outputFile;
+std::fstream inputFile;
+// Entry point
+int main() {
+	outputFile.open("infinifile.txt");
+}
